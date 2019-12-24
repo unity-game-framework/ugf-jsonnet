6850b733e13a83992f9e818a24fd045078997622
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2019-12-24 21:06:28 +0200

    Update JsonNetUtility

---
+++ Packages/UGF.JsonNet/Runtime/JsonNetContractResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace UGF.JsonNet.Runtime
+{
+    public class JsonNetContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var properties = (List<JsonProperty>)base.CreateProperties(type, memberSerialization);
+
+            properties.Sort(JsonNetPropertyInheritanceCountComparer.Default);
+
+            return properties;
+        }
+
+        protected override JsonDictionaryContract CreateDictionaryContract(Type objectType)
+        {
+            JsonDictionaryContract contract = base.CreateDictionaryContract(objectType);
+
+            contract.DictionaryKeyResolver = name => name;
+
+            return contract;
+        }
+    }
+}
--- Packages/UGF.JsonNet/Runtime/JsonNetUtility.cs
+++ Packages/UGF.JsonNet/Runtime/JsonNetUtility.cs
@@ -1,6 +1,47 @@
-﻿namespace UGF.JsonNet.Runtime
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace UGF.JsonNet.Runtime
 {
     public static class JsonNetUtility
     {
+        public static JsonSerializerSettings DefaultSettings { get; } = new JsonSerializerSettings
+        {
+            ContractResolver = new JsonNetContractResolver(),
+            Formatting = Formatting.Indented,
+            TypeNameHandling = TypeNameHandling.Auto,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+        public static string ToJson(object target, JsonSerializerSettings settings = null)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            return JsonConvert.SerializeObject(target, settings ?? DefaultSettings);
+        }
+
+        public static T FromJson<T>(string text, JsonSerializerSettings settings = null)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
+
+            return JsonConvert.DeserializeObject<T>(text, settings ?? DefaultSettings);
+        }
+
+        public static string Format(string text, Formatting formatting = Formatting.None)
+        {
+            using (var reader = new StringReader(text))
+            using (var writer = new StringWriter())
+            {
+                var jsonReader = new JsonTextReader(reader);
+                var jsonWriter = new JsonTextWriter(writer) { Formatting = formatting };
+
+                jsonWriter.WriteToken(jsonReader);
+
+                return writer.ToString();
+            }
+        }
     }
 }
